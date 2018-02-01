94f64f8ce01dce0d4c626e2e17725861ea55ca4e
Janus <ysangkok@gmail.com>
2018-02-01 12:02:42 +0100

    asyncio: remove lol.py test script

--- lol.py
+++
@@ -1,9 +0,0 @@
-from lib import bitcoin, transaction
-import binascii
-
-bitcoin.set_simnet()
-
-print(transaction.Transaction.pay_script(bitcoin.TYPE_ADDRESS, "sb1q3hmm6ehggew56pz06km429mxeg3jhwtj8yfgk5"))
-scr = bitcoin.address_to_script("sb1q3hmm6ehggew56pz06km429mxeg3jhwtj8yfgk5")
-print(scr)
-print(len(scr))
