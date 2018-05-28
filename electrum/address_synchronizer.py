4312b2adf1b9d86284050ebc53cfbc30dc73d3a4
ThomasV <thomasv@electrum.org>
2018-05-28 09:39:05 +0200

    lightning: move lnworker code to its own module

--- electrum/address_synchronizer.py
+++ electrum/address_synchronizer.py
@@ -133,7 +133,7 @@ class AddressSynchronizer(PrintError):
             network.add_jobs([self.verifier, self.synchronizer])
             if network.config.get("lnbase", False):
                 asyncio.set_event_loop(network.asyncio_loop)
-                from .lnbase import LNWorker
+                from .lnworker import LNWorker
                 self.lnworker = LNWorker(self, network)
         else:
             self.verifier = None
