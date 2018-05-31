4c2f6affcfc0d6f145de43258d4b0bef4c0c96f9
ThomasV <thomasv@electrum.org>
2018-05-31 14:05:09 +0200

    lightning: connect send button

--- electrum/gui/qt/paytoedit.py
+++ electrum/gui/qt/paytoedit.py
@@ -55,6 +55,7 @@ class PayToEdit(CompletionTextEdit, ScanQRTextEdit):
         self.errors = []
         self.is_pr = False
         self.is_alias = False
+        self.is_lightning = False
         self.update_size()
         self.payto_address = None
         self.previous_payto = ''
@@ -128,6 +129,7 @@ class PayToEdit(CompletionTextEdit, ScanQRTextEdit):
                 return
             if data.startswith("ln"):
                 self.win.parse_lightning_invoice(data)
+                self.lightning_invoice = data
                 return
             try:
                 self.payto_address = self.parse_output(data)
